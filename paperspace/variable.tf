949c9a68b6fc586d8232be5af1e7b1e55c675801
Dusan-Scripted <dusan@scripted.rs>
2023-05-04 10:27:46 +0000

    From code - Edit vMix servers list

--- paperspace/variable.tf
+++ paperspace/variable.tf
@@ -1,4 +1,4 @@
 	variable "vmix_names" {
-	default = ["vmix7","vmix8","vmix9","vmix10","vmix11","vmix12","vmix13"]
+	default = ["vmix7","vmix8","vmix9","vmix10","vmix11","vmix12","vmix13","vmix14"]
 	type= wqe21e 
 }
